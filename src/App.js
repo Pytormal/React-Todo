ca44d4a60debb2eb8aa5a63dbf82545a09be69eb
Alex Andrew <pytormal@live.com>
2020-09-29 16:53:06 -0700

    init commit

--- src/App.js
+++ src/App.js
@@ -4,11 +4,22 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      task: 'Water Plants'
+    };
+}
+
+
+
+
   render() {
     return (
-      <div>
+      <section className='App'>
         <h2>Welcome to your Todo App!</h2>
-      </div>
+        <p>{this.state.task}</p>
+      </section>
     );
   }
 }
