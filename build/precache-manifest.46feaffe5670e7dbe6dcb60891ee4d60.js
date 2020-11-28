e1029e196da0dc217cd0756e47856facec6c23ed
Alex Andrew <pytormal@live.com>
2020-11-28 15:57:01 -0800

    removed /build from .gitignore

---
+++ build/precache-manifest.46feaffe5670e7dbe6dcb60891ee4d60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a8e64562db9a1b15200bfb83b808da5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1943f21153e3b5d0b97b",
+    "url": "/static/css/main.8bfccc9c.chunk.css"
+  },
+  {
+    "revision": "463cb69b53f6364c5241",
+    "url": "/static/js/2.7f389b90.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.7f389b90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1943f21153e3b5d0b97b",
+    "url": "/static/js/main.257de01a.chunk.js"
+  },
+  {
+    "revision": "47a322c7b2dcb2a0402a",
+    "url": "/static/js/runtime-main.c5fb7bee.js"
+  }
+]);
